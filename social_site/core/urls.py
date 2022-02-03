4aef5ed7bc84d24a895ae541f21c147df093f7e9
Cristiano <kcrisapp@gmail.com>
2022-02-03 07:29:15 +0100

    funzione cerca

--- social_site/core/urls.py
+++ social_site/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view() , name='homepage'),
     path('users/', views.UserListView.as_view() , name='users_list'),
+    path('cerca/', views.cerca , name='funzione_cerca'),
     path('user/<str:username>/', views.user_profile_view , name='user_profile'),
     
 ]
--- social_site/core/views.py
+++ social_site/core/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.models import User
 from django.views.generic.list import ListView
-from forum.models import Discussione, Sezione
+from forum.models import Discussione, Sezione, Post
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 # def homepage(request):
 #     return render(request, 'core/homepage.html')
@@ -25,5 +26,20 @@ def user_profile_view(request, username):
     return render(request, "core/user_profile.html", context)
 
 
+#funzione cerca
+def cerca(request):
+    if "q" in request.GET:
+        querystring = request.GET.get("q")
+        print(querystring)
+        if len(querystring) == 0:
+            return redirect("/cerca/")
+        discussioni = Discussione.objects.filter(titolo__icontains=querystring)
+        posts = Post.objects.filter(contenuto__icontains=querystring)
+        users = User.objects.filter(username__icontains=querystring)
+        context = {'discussioni': discussioni, 'posts': posts, 'users':users}
+
+        return render(request, 'core/cerca.html', context)
+    else:
+        return render(request,'core/cerca.html')
 
 
